55cbc225f1fbf905609f442dc540d90e6a2fa8af
大澤健人 <oosawataketo@oosawataketonoMacBook-Air.local>
2024-06-27 14:27:43 +0900

    urlを処理できるコードを追加

--- 00_my_first_app.py
+++ 00_my_first_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_chat import message
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     SystemMessage,
@@ -7,13 +8,17 @@ from langchain.schema import (
 )
 from langchain.callbacks import get_openai_callback
 
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
 
 def init_page():
     st.set_page_config(
-        page_title="My Great ChatGPT",
+        page_title="Website Summarizer",
         page_icon="🤗"
     )
-    st.header("My Great ChatGPT 🤗")
+    st.header("Website Summarizer 🤗")
     st.sidebar.title("Options")
 
 
@@ -33,11 +38,50 @@ def select_model():
     else:
         model_name = "gpt-4"
 
-    # スライダーを追加し、temperatureを0から2までの範囲で選択可能にする
-    # 初期値は0.0、刻み幅は0.01とする
-    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+    return ChatOpenAI(temperature=0, model_name=model_name)
 
-    return ChatOpenAI(temperature=temperature, model_name=model_name)
+
+def get_url_input():
+    url = st.text_input("URL: ", key="input")
+    return url
+
+
+def validate_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def get_content(url):
+    try:
+        with st.spinner("Fetching Content ..."):
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # fetch text from main (change the below code to filter page)
+            if soup.main:
+                return soup.main.get_text()
+            elif soup.article:
+                return soup.article.get_text()
+            else:
+                return soup.body.get_text()
+    except:
+        st.write('something wrong')
+        return None
+
+
+def build_prompt(content, n_chars=300):
+    return f"""以下はとある。Webページのコンテンツである。内容を{n_chars}程度でわかりやすく要約してください。
+
+========
+
+{content[:1000]}
+
+========
+
+日本語で書いてね！
+"""
 
 
 def get_answer(llm, messages):
@@ -52,24 +96,33 @@ def main():
     llm = select_model()
     init_messages()
 
-    # ユーザーの入力を監視
-    if user_input := st.chat_input("聞きたいことを入力してね！"):
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("ChatGPT is typing ..."):
-            answer, cost = get_answer(llm, st.session_state.messages)
-        st.session_state.messages.append(AIMessage(content=answer))
-        st.session_state.costs.append(cost)
+    container = st.container()
+    response_container = st.container()
 
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message('assistant'):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message('user'):
-                st.markdown(message.content)
-        else:  # isinstance(message, SystemMessage):
-            st.write(f"System message: {message.content}")
+    with container:
+        url = get_url_input()
+        is_valid_url = validate_url(url)
+        if not is_valid_url:
+            st.write('Please input valid url')
+            answer = None
+        else:
+            content = get_content(url)
+            if content:
+                prompt = build_prompt(content)
+                st.session_state.messages.append(HumanMessage(content=prompt))
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = get_answer(llm, st.session_state.messages)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+
+    if answer:
+        with response_container:
+            st.markdown("## Summary")
+            st.write(answer)
+            st.markdown("---")
+            st.markdown("## Original Text")
+            st.write(content)
 
     costs = st.session_state.get('costs', [])
     st.sidebar.markdown("## Costs")
